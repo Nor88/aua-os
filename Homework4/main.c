c4194124d64946e1e07ff8bb27da626617fbfb02
Nor88 <norayr_gevorgyan@edu.aua.am>
2025-09-24 12:24:50 +0000

    Hw4 is Done!

---
+++ Homework4/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "math_utils.h"
+
+int main(void) {
+    int x = 7;
+    int s = square(x);
+    printf("square(%d) = %d\n", x, s);
+    return 0;
+}
+
---
+++ Homework4/math_utils.h
@@ -0,0 +1,7 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+int square(int n);
+
+#endif // MATH_UTILS_H
+
